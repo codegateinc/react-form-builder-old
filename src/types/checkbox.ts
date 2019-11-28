63f607493982dbcf925822d8eccc31ea45ec514a
Grzegorz Tarnopolski <tarnopolskigrzegorz@gmail.com>
2019-11-28 11:05:07 +0100

    improved checkbox

--- src/types/checkbox.ts
+++ src/types/checkbox.ts
@@ -7,7 +7,7 @@ export type CheckboxProps = {
     isSelected?: boolean,
     withError?: string,
     formFieldName?: string,
-    onClick: OnCheckboxChange,
+    onClick?: OnCheckboxChange,
     renderComponent?: RenderCheckboxComponent,
     errorMessageStyles?: React.CSSProperties
 }
