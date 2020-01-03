030fa97f5e47bd7266d437f68055f0b8341a7669
Grzegorz Tarnopolski <tarnopolskigrzegorz@gmail.com>
2020-01-03 15:17:36 +0100

    fixes

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -511,8 +511,6 @@ export class Form<T> extends React.Component<FormProps<T>, FormState> {
                 ? formFieldConfigProps.compareWith && formFieldConfigProps.compareWith.errorMessage
                 : undefined
 
-        console.log(errorMessage, this.getFieldErrorMessage(fieldName, currentValue))
-
         const isPristine = !(currentValue !== (this.props.formConfig[fieldName] as FormInputConfigProps).value)
 
         this.updateState({
